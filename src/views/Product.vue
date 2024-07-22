a2a0613942d45d30920ee78f36d4c424866a0106
twingqywingqy <rendra.zurd04@gmail.com>
2024-07-23 01:04:30 +0700

    feat: ekstrak dan mendapatkan data dari route

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,5 +1,19 @@
+<script setup>
+import { useRoute } from "vue-router";
+import products from "../data/products.json";
+
+const route = useRoute();
+const product = products.find(
+	(product) => product.id === parseInt(route.params.id)
+);
+</script>
+
 <template>
 	<div>
 		<h1>Product</h1>
+		<div>
+			<h2>{{ product.name }} - {{ product.price }}</h2>
+			<p>Year: {{ product.year }}</p>
+		</div>
 	</div>
 </template>
