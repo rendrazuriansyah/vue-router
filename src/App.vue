533f3c12511028a77bfc4340a355fd0f911f7ad1
twingqywingqy <rendra.zurd04@gmail.com>
2024-07-22 10:02:57 +0700

    feat: menggunakan RouterLink untuk berpindah halaman

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import { ref } from "vue";
 import { RouterView } from "vue-router";
+import Nav from "./components/Nav.vue";
+
+const count = ref(0);
 </script>
 
 <template>
+	<Nav />
+	<button @click="count++">Count: {{ count }}</button>
 	<main>
 		<RouterView />
 	</main>
---
+++ src/components/Nav.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+	<div>
+		<!-- Hyperlink: pindah page refresh ulang -->
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		|
+		<!-- Routerlink: pindah page tanpa refresh -->
+		<RouterLink to="/">Home</RouterLink>
+		<RouterLink to="/about">About</RouterLink>
+	</div>
+</template>
+
+<style>
+div {
+	display: flex;
+	gap: 20px;
+}
+</style>
